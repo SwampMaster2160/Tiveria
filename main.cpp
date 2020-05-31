0264c341d028a7da7b2ef5bacabdabd9556ab07e
SwampMaster2160 <62127482+SwampMaster2160@users.noreply.github.com>
2020-06-01 11:36:48 +1200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -42,28 +42,28 @@ uint64_t time_ = 0;
 NPC player;
 MapTile map[0x100][0x100];
 
+//sf::Texture grass;
+//sf::Texture water;
+sf::Texture textures;
+
+sf::Sprite sprite;
+
 int WinMain()
 {
-    //time_ = 0;
-
     player = NPC(0x0000, {0, 0});
-
-    //playerPos = { 0x00, 0x00 };
-    //playerWalkOffset = { 0x00, 0x00 };
 
     for (uint16_t x = 0; x < 256; x++)
     {
         for (uint16_t y = 0; y < 256; y++)
         {
-            map[x][y].image = rand() % 2;//rand() % 2
+            map[x][y].image = rand() % 2 + 1;//rand() % 2
             if (x == y || x == 0 || y == 0)
             {
-                map[x][y].image = 2;
+                map[x][y].image = 0;
             }
         }
     }
 
-    //screenSize = sf::VideoMode::GetDesktopMode();
     sf::RenderWindow window(sf::VideoMode(512, 256), "RPG", sf::Style::Fullscreen);//sf::Style::Fullscreen
     window.setFramerateLimit(60);
     window.setVerticalSyncEnabled(true);
@@ -71,15 +71,18 @@ int WinMain()
 
     screenAspectRatio = (double)screenRes.x / (double)screenRes.y;
     screenOffset = (screenRes.x - (screenRes.y * 2)) / 2;
-    //screenOffset = -100;
+
+    //grass.loadFromFile("assets/textures/grass.png");
+    //water.loadFromFile("assets/textures/water.png");
+    textures.loadFromFile("assets/textures/textures.png");
+
+    sprite.setTexture(textures);
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            //if (event.type == sf::Event::Closed)
-            //    window.close();
             switch (event.type)
             {
             case sf::Event::Closed:
@@ -112,17 +115,20 @@ int WinMain()
         rectangle.setFillColor(sf::Color::Black);
         window.draw(rectangle);
 
-        rectangle.setSize(sf::Vector2f(screenRes.y / 16, screenRes.y / 16));
+        //rectangle.setSize(sf::Vector2f(screenRes.y / 16, screenRes.y / 16));
+
+        sprite.setScale((double)screenRes.y / 256, (double)screenRes.y / 256);
         for (uint8_t x = 0; x < 32; x++)
         {
             for (uint8_t y = 0; y < 16; y++)
             {
-                rectangle.setPosition(x * (screenRes.y / 16) + screenOffset, y * (screenRes.y / 16));
-                rectangle.setFillColor(sf::Color::Green);
-                if (map[(uint8_t)(x - 16 + player.pos.x)][(uint8_t)(y - 8 + player.pos.y)].image > 0) rectangle.setFillColor(sf::Color::Blue);
-                if (map[(uint8_t)(x - 16 + player.pos.x)][(uint8_t)(y - 8 + player.pos.y)].image == 2) rectangle.setFillColor(sf::Color::Black);
-                if (x == 16 && y == 8) rectangle.setFillColor(sf::Color::White);
-                window.draw(rectangle);
+                uint16_t image = map[(uint8_t)(x - 16 + player.pos.x)][(uint8_t)(y - 8 + player.pos.y)].image;
+
+                sprite.setTextureRect(sf::Rect<int>((image % 256) % 16 * 16 + (floor((image % 4096) / 256) * 256), floor((image % 256) / 16) * 16 + floor(image / 4096) * 256, 16, 16));
+
+                sprite.setPosition(x * (screenRes.y / 16) + screenOffset, y * (screenRes.y / 16));
+
+                window.draw(sprite);
             }
         }
 
